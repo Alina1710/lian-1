b7b5a1dc8fc99f72fa1c926711756d912643ca61
Alina <alina.m1710@gmail.com>
2020-06-23 14:12:28 +0300

    Init

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        dots: true,
+    })
+})
+
+$('.mobile-menu').click(function() {
+    $(this).toggleClass('active-menu')
+    $('.mobile-list').slideToggle('fast')
+})
