5c4b4786dc3e8a9f6f1e47917b8fae6debbaed52
Alina <alina.m1710@gmail.com>
2020-08-13 16:44:32 +0300

    13

--- src/js/index.js
+++ src/js/index.js
@@ -6,17 +6,58 @@ $(document).ready(function() {
         fade: true,
         dots: true,
     })
-})
 
-$('.mobile-menu').click(function() {
-    $(this).toggleClass('active-menu')
-    $('.mobile-list').slideToggle('fast')
-})
+    $('.mobile-menu').click(function() {
+        $(this).toggleClass('active-menu')
+        $('.mobile-list').slideToggle('fast')
+    })
 
-$('.pin-close').click(function() {
-    $('.map-pin-text').css('display', 'none')
-})
+    $('.pin-close').click(function() {
+        $('.map-pin-text').css('display', 'none')
+    })
 
-$('.contact-map-img').click(function() {
-    $('.map-pin-text').css('display', 'block')
+    $('.contact-map-img').click(function() {
+        $('.map-pin-text').css('display', 'block')
+    })
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav',
+    })
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 580,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 2,
+                },
+            },
+        ],
+    })
+    $('.portfolio-details-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+    })
 })
